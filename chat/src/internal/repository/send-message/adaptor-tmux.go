25c1e337c971b8f19422fbdecde3768274c752b3
Nuttawit Butsara <nuttawit@gsbitmanagement.co.th>
2026-05-15 15:34:36 +0700

    fix chat tmux message submit

--- chat/src/internal/repository/send-message/adaptor-tmux.go
+++ chat/src/internal/repository/send-message/adaptor-tmux.go
@@ -3,6 +3,7 @@ package sendmessage
 import (
 	"fmt"
 	"os/exec"
+	"time"
 )
 
 type adaptorTmux struct {
@@ -14,8 +15,17 @@ func NewAdaptorTmux() Port {
 
 func (a *adaptorTmux) Execute(request *Request) (*Response, error) {
 	for _, v := range request.To {
-		cmd := exec.Command("tmux", "send-keys", "-t", v.Fleet, fmt.Sprintf("#inbox: from [%s] => %s", request.From, request.Message), "C-m")
-		cmd.Output()
+		message := fmt.Sprintf("#inbox: from [%s] => %s", request.From, request.Message)
+
+		if err := exec.Command("tmux", "send-keys", "-t", v.Fleet, "-l", message).Run(); err != nil {
+			return nil, err
+		}
+
+		time.Sleep(50 * time.Millisecond)
+
+		if err := exec.Command("tmux", "send-keys", "-t", v.Fleet, "Enter").Run(); err != nil {
+			return nil, err
+		}
 	}
 
 	return &Response{}, nil
